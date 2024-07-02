3f21a304acb2daed1b093f3957cf0d85e3abd0e6
walaa mahmoud <walaamahmoud4567@gmail.com>
2024-07-03 02:24:26 +0300

    first commit

---
+++ js/human.js
@@ -0,0 +1,23 @@
+
+
+let x=10;
+let y=40;
+let z=30;
+
+export class Hunans {
+
+    constructor(name, age, adores) {
+        this.name = name;
+        this.age = age;
+        this.adores = adores;
+    }
+    wellcom() {
+        console.log('wellcom')
+    }
+}
+
+export {
+    x,
+    y,
+    z
+}
